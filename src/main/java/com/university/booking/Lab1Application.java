ab5755834c66432d36708d98d7ae8105d1aff3d8
vladimirugol <vladimirugol66@gmail.com>
2026-09-06 16:12:49 +0300

    repo+dependencies

---
+++ src/main/java/com/university/booking/Lab1Application.java
@@ -0,0 +1,14 @@
+package com.university.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class Lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+    }
+}
